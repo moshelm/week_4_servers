aaba3aa786e522e4a13b5742cd5ec6703d2f79df
Moshe <moshe@example.com>
2025-11-13 12:44:44 +0200

    finish all

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from fastapi import FastAPI
 import uvicorn
 from pydantic import BaseModel
-import json
 
 
 app = FastAPI()
 
-all_char = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+all_chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-class ItemBody(BaseModel):
+class ItemBodyCaesar(BaseModel):
     text:str
     offset:int
     mode:str
 
+class ItemBodyFence(BaseModel):
+    text:str
+
 @app.get('/test')
-def for_checking():
+def checking_point():
     return {"msg":"hi from text"}
 
 @app.get('/test/{name}')
@@ -22,49 +24,67 @@ def add_user(name:str):
     with open('names.txt','a') as f:
         f.write(name + " ")
         f.write(' ')
-    f.close()
     return {"msg":"saved user"}
 
 @app.post('/caesar')
-def handel_caesar_cipher(body:ItemBody):
+def caesar_cipher_management(body:ItemBodyCaesar):
     if body.mode == 'encrypt':
-        encrypt_text =encrypted_by_caesar(body.text,body.offset)
-        return {'encrypted_text':encrypt_text}
+        encrypted_text =caesar_cipher_encryption(body.text,body.offset)
+        return {'encrypted_text':encrypted_text}
     else:
-        decoded_text = decrypted_by_caesar(body.text,body.offset)
+        decoded_text = caesar_cipher_decryption(body.text, body.offset)
         return {'decrypted':decoded_text}
 
+@app.get('/fence/encrypt')
+def encrypted_fence_cipher(text:str):
+    return {'encrypted_text':fence_cipher_encryption(text)}
 
+def fence_cipher_encryption(text:str):
+    text_no_space = text.replace(' ', '')
+    encrypted_text = text_no_space[::2] + text_no_space[1::2]
+    return encrypted_text
 
+@app.post('/fence/decrypt')
+def decrypted_fence_cipher(body:ItemBodyFence):
+    return {'decrypted': fence_cipher_decryption(body.text)}
 
-@app.get('fence/encrypt')
-def encrypted_by_fence(text:str):
-    return {'encrypted_text':text}
+def fence_cipher_decryption(text:str):
+    # The boundary between even and odd
+    limit = len(text)//2 + len(text) % 2
+    # Division by type
+    even_text = text[:limit]
+    odd_text = text[limit:]
 
-@app.post('fence/decrypt')
-def decrypted_by_fence(body:ItemBody):
-    return {'decrypted': body.text}
+    decoded_text = []
 
-def decrypted_by_caesar(text:str,offset:int):
+    for index in range(len(even_text)):
+        decoded_text.append(even_text[index])
+        # test ot of range, finish odd_text
+        if len(odd_text) > index:
+            decoded_text.append(odd_text[index])
+    return ''.join(decoded_text)
+
+def caesar_cipher_decryption(text:str, offset:int):
     decoded_text = []
     for char in text:
         if char.isalpha():
-            decoded_text.append(all_char[(all_char.index(char) - offset) % 26])
+            # find the original char in all chars by calculate index - offset and module 26
+            decoded_text.append(all_chars[(all_chars.index(char) - offset) % 26])
         else:
+            # if char is space
             decoded_text.append(char)
     return ''.join(decoded_text)
 
-def encrypted_by_caesar(text:str,offset):
+def caesar_cipher_encryption(text:str,offset):
     crypt_text = []
     for char in text:
         if char.isalpha():
-            crypt_text.append(all_char[(all_char.index(char)+offset) % 26])
+            # find the original char in all chars by calculate index + offset and module 26
+            crypt_text.append(all_chars[(all_chars.index(char) + offset) % 26])
         else:
+            # if char is space
             crypt_text.append(char)
     return ''.join(crypt_text)
 
 if __name__ == '__main__':
     uvicorn.run(app,host='localhost',port=8000)
-    # print(encrypted_by_caesar('hello world',7))
-    # print(decrypted_by_caesar('mubsecu aetaet' ,16))
-    #
