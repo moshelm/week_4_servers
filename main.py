9c0892efceeb31be7934403102a68b1b519436ad
Moshe <moshe@example.com>
2025-11-13 11:48:45 +0200

    finish caesar

--- main.py
+++ main.py
@@ -3,28 +3,39 @@ import uvicorn
 from pydantic import BaseModel
 import json
 
+
 app = FastAPI()
+
+all_char = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 class ItemBody(BaseModel):
     text:str
     offset:int
-    mode:""
+    mode:str
 
 @app.get('/test')
 def for_checking():
     return {"msg":"hi from text"}
 
-@app.get('/test/')
+@app.get('/test/{name}')
 def add_user(name:str):
     with open('names.txt','a') as f:
-        f.write(name)
+        f.write(name + " ")
+        f.write(' ')
+    f.close()
+    return {"msg":"saved user"}
 
 @app.post('/caesar')
 def handel_caesar_cipher(body:ItemBody):
     if body.mode == 'encrypt':
-        return {'encrypted_text':body.text}
+        encrypt_text =encrypted_by_caesar(body.text,body.offset)
+        return {'encrypted_text':encrypt_text}
     else:
-        return {'decrypted':body.text}
+        decoded_text = decrypted_by_caesar(body.text,body.offset)
+        return {'decrypted':decoded_text}
+
+
+
 
 @app.get('fence/encrypt')
 def encrypted_by_fence(text:str):
@@ -34,6 +45,26 @@ def encrypted_by_fence(text:str):
 def decrypted_by_fence(body:ItemBody):
     return {'decrypted': body.text}
 
+def decrypted_by_caesar(text:str,offset:int):
+    decoded_text = []
+    for char in text:
+        if char.isalpha():
+            decoded_text.append(all_char[(all_char.index(char) - offset) % 26])
+        else:
+            decoded_text.append(char)
+    return ''.join(decoded_text)
+
+def encrypted_by_caesar(text:str,offset):
+    crypt_text = []
+    for char in text:
+        if char.isalpha():
+            crypt_text.append(all_char[(all_char.index(char)+offset) % 26])
+        else:
+            crypt_text.append(char)
+    return ''.join(crypt_text)
 
 if __name__ == '__main__':
-    uvicorn.run(app,host='localhost',port=8000 )
+    uvicorn.run(app,host='localhost',port=8000)
+    # print(encrypted_by_caesar('hello world',7))
+    # print(decrypted_by_caesar('mubsecu aetaet' ,16))
+    #
